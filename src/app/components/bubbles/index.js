bd726afd2c3e0f8b032c4e2cf6ceeed2fe0ce4ac
Pedro Henestroza <soype@192.168.1.22>
2024-03-07 12:56:07 -0300

    Bubble bubble

---
+++ src/app/components/bubbles/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './bubbles.module.scss';
+
+const Bubbles = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.b1}></div>
+        <div className={styles.b2}></div>
+        <div className={styles.b3}></div>
+        <div className={styles.b4}></div>
+        <div className={styles.b5}></div>
+    </div>
+  )
+}
+
+export default Bubbles;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 
 import styles from "./page.module.scss";
 
+import Bubbles from "./components/bubbles";
+
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.container}></div>
+      <Bubbles></Bubbles>
     </main>
   );
 }
