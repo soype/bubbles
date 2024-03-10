66b04a7bb8ed58fd9da415f3c82ef272b58e8343
Pedro Henestroza <soype@192.168.1.17>
2024-03-09 21:06:15 -0300

    Add use client

--- src/app/components/bubbles/index.js
+++ src/app/components/bubbles/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
+"use client";
 
-import styles from './bubbles.module.scss';
+import React, { useState } from "react";
+
+import styles from "./bubbles.module.scss";
 
 const Bubbles = () => {
   return (
     <div className={styles.container}>
-        <div className={styles.b1}></div>
-        <div className={styles.b2}></div>
-        <div className={styles.b3}></div>
-        <div className={styles.b4}></div>
-        <div className={styles.b5}></div>
+      <div className={styles.b1}></div>
+      <div className={styles.b2}></div>
+      <div className={styles.b3}></div>
+      <div className={styles.b4}></div>
+      <div className={styles.b5}></div>
     </div>
-  )
-}
+  );
+};
 
 export default Bubbles;
