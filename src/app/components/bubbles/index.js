d5179378c78a9949ad6634e9ea7af6ea9a1f8b20
Pedro Henestroza <soype@pop-os.localdomain>
2024-03-11 23:05:57 -0300

    Improve bubbles and make mobile

--- src/app/components/bubbles/index.js
+++ src/app/components/bubbles/index.js
@@ -1,18 +1,21 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import styles from "./bubbles.module.scss";
 
-const Bubbles = () => {
+const Bubbles = ( { color } ) => {
+
   return (
     <div className={styles.container}>
       <h2>Bubble Bubble</h2>
-      <div className={styles.b1}></div>
-      <div className={styles.b2}></div>
-      <div className={styles.b3}></div>
-      <div className={styles.b4}></div>
-      <div className={styles.b5}></div>
+    <div className={styles['bubble-container']} id="bubbleContainer">
+      <div className={styles.b1} id="blue"></div>
+      <div className={styles.b2} id="purple"></div>
+      <div className={styles.b3} id="blue-lt"></div>
+      <div className={styles.b4} id="red"></div>
+      <div className={styles.b5} id="yellow"></div>
+    </div>
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,31 @@
+'use client'
+
+import React, { useState } from 'react';
 
 import styles from "./page.module.scss";
 
 import Bubbles from "./components/bubbles";
 
 export default function Home() {
+
+  const [color, setColor] = useState('blue')
+
+  const colorSelectHandler = (e) => {
+    console.log(e.target.value)
+  }
+
   return (
     <main className={styles.main}>
-      <Bubbles></Bubbles>
+      <Bubbles color={color}></Bubbles>
+      <div className={styles['color-selector']}>
+        {/* <select id="colorSelect" onChange={colorSelectHandler}>
+          <option value="blue">Blue</option>
+          <option value="red">Red</option>
+          <option value="yellow">Yellow</option>
+          <option value="blue-lt">Light blue</option>
+          <option value="purple">Purple</option>
+        </select> */}
+      </div>
     </main>
   );
 }
